20aef636d777a433018ee9d02f143f5dd59f526d
Ken Gilmer <kgilmer@gmail.com>
2020-01-15 20:27:08 -0800

    Fixes to package model.

--- back-copy.sh
+++ back-copy.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-
+# This script is for copying packages across Ubuntu versions.
 
 back_copy() {
     for PACKAGE in ${PACKAGES//,/ }; do
@@ -12,5 +12,6 @@ SOURCE_PPA="~regolith-linux/ubuntu/unstable"
 TARGET_PPA="~regolith-linux/ubuntu/unstable"
 SOURCE_VERSION="eoan"
 TARGET_VERSION="bionic"
-PACKAGES="fonts-source-code-pro-ttf,moka-icon-theme,paper-icon-theme"
+PACKAGES="regolith-compositor-compton-glx,regolith-compositor-none,regolith-compositor-xcompmgr,regolith-desktop,regolith-ftue,regolith-gdm3-theme,regolith-i3-gaps-config,regolith-i3xrocks-config,regolith-st,regolith-styles,regolith-rofication,remontoire,cahuella,fonts-source-code-pro-ttf,moka-icon-theme,paper-icon-theme"
+
 back_copy
