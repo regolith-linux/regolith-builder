aad9209dc18884dd77ceed1e3b55b07ed2323ab1
Ken Gilmer <kgilmer@gmail.com>
2020-09-12 12:13:20 -0700

    Add experimental scripts for generating ISOs via automation.

---
+++ chroot-script.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+set -x 
+
+mount none -t proc /proc
+mount none -t sysfs /sys
+mount none -t devpts /dev/pts
+export HOME=/root
+export LC_ALL=C
+
+echo "ubuntu-fs-live" > /etc/hostname
+
+cat <<EOF > /etc/apt/sources.list
+deb http://us.archive.ubuntu.com/ubuntu/ bionic main restricted universe multiverse 
+deb-src http://us.archive.ubuntu.com/ubuntu/ bionic main restricted universe multiverse
+deb http://us.archive.ubuntu.com/ubuntu/ bionic-security main restricted universe multiverse 
+deb-src http://us.archive.ubuntu.com/ubuntu/ bionic-security main restricted universe multiverse
+deb http://us.archive.ubuntu.com/ubuntu/ bionic-updates main restricted universe multiverse 
+deb-src http://us.archive.ubuntu.com/ubuntu/ bionic-updates main restricted universe multiverse    
+EOF
+
+apt-get update
+
+apt-get install -y systemd-sysv
+
+dbus-uuidgen > /etc/machine-id
+ln -fs /etc/machine-id /var/lib/dbus/machine-id
+
+dpkg-divert --local --rename --add /sbin/initctl
+ln -s /bin/true /sbin/initctl
+
+apt-get install -y \
+    ubuntu-standard \
+    casper \
+    lupin-casper \
+    discover \
+    laptop-detect \
+    os-prober \
+    network-manager \
+    resolvconf \
+    net-tools \
+    wireless-tools \
+    wpagui \
+    locales \
+    linux-generic
+
+apt-get install -y \
+    ubiquity \
+    ubiquity-casper \
+    ubiquity-frontend-gtk \
+    ubiquity-slideshow-ubuntu \
+    ubiquity-ubuntu-artwork
+
+apt-get install -y \
+    plymouth-theme-ubuntu-logo \
+    ubuntu-gnome-desktop \
+    ubuntu-gnome-wallpapers
+
+apt-get install -y \
+    clamav-daemon \
+    terminator \
+    apt-transport-https \
+    curl \
+    vim \
+    nano \
+    less
+
+apt-get purge -y \
+     transmission-gtk \
+     transmission-common \
+     gnome-mahjongg \
+     gnome-mines \
+     gnome-sudoku \
+     aisleriot \
+     hitori
+
+apt-get autoremove -y
+
+dpkg-reconfigure locales
+
+dpkg-reconfigure resolvconf
+
+cat <<EOF > /etc/NetworkManager/NetworkManager.conf
+[main]
+rc-manager=resolvconf
+plugins=ifupdown,keyfile
+dns=dnsmasq
+[ifupdown]
+managed=false
+EOF
+
+dpkg-reconfigure network-manager
+
+truncate -s 0 /etc/machine-id
+
+rm /sbin/initctl
+dpkg-divert --rename --remove /sbin/initctl
+
+apt-get clean
+rm -rf /tmp/* ~/.bash_history
+umount /proc
+umount /sys
+umount /dev/pts
+export HISTSIZE=0
---
+++ iso-generator.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+set -x 
+set -e
+
+ISO_ROOT=$1
+
+if [ -z $ISO_ROOT ]; then
+  echo "Usage: iso-generator.sh [ISO root directory]"
+  exit 1
+fi
+
+if [ -d "$ISO_ROOT" ]; then
+  echo "$ISO_ROOT already exists.  Must specify non-existent target."
+  exit 1
+fi
+
+echo "Generating ISO in $ISO_ROOT..."
+
+sudo apt-get install \
+    binutils \
+    debootstrap \
+    squashfs-tools \
+    xorriso \
+    grub-pc-bin \
+    grub-efi-amd64-bin \
+    mtools
+    
+mkdir $ISO_ROOT
+
+sudo debootstrap \
+    --arch=amd64 \
+    --variant=minbase \
+    bionic \
+    $ISO_ROOT/chroot \
+    http://us.archive.ubuntu.com/ubuntu/
+
+sudo cp ./chroot-script.sh $ISO_ROOT/chroot/
+
+sudo mount --bind /dev $ISO_ROOT/chroot/dev
+sudo mount --bind /run $ISO_ROOT/chroot/run
+
+sudo chroot $ISO_ROOT/chroot ./chroot-script.sh
+
+sudo umount $ISO_ROOT/chroot/dev
+sudo umount $ISO_ROOT/chroot/run
+
+cd $ISO_ROOT
+mkdir -p image/{casper,isolinux,install}
+
+sudo cp chroot/boot/vmlinuz-**-**-generic image/casper/vmlinuz
+sudo cp chroot/boot/initrd.img-**-**-generic image/casper/initrd
+sudo cp chroot/boot/memtest86+.bin image/install/memtest86+
+
+wget --progress=dot https://www.memtest86.com/downloads/memtest86-usb.zip -O image/install/memtest86-usb.zip
+unzip -p image/install/memtest86-usb.zip memtest86-usb.img > image/install/memtest86
+rm image/install/memtest86-usb.zip
+
+touch image/ubuntu
+
+cat <<EOF > image/isolinux/grub.cfg
+
+search --set=root --file /ubuntu
+
+insmod all_video
+
+set default="0"
+set timeout=30
+
+menuentry "Try Ubuntu FS without installing" {
+   linux /casper/vmlinuz boot=casper quiet splash ---
+   initrd /casper/initrd
+}
+
+menuentry "Install Ubuntu FS" {
+   linux /casper/vmlinuz boot=casper only-ubiquity quiet splash ---
+   initrd /casper/initrd
+}
+
+menuentry "Check disc for defects" {
+   linux /casper/vmlinuz boot=casper integrity-check quiet splash ---
+   initrd /casper/initrd
+}
+
+menuentry "Test memory Memtest86+ (BIOS)" {
+   linux16 /install/memtest86+
+}
+
+menuentry "Test memory Memtest86 (UEFI, long load time)" {
+   insmod part_gpt
+   insmod search_fs_uuid
+   insmod chain
+   loopback loop /install/memtest86
+   chainloader (loop,gpt1)/efi/boot/BOOTX64.efi
+}
+EOF
+
+sudo chroot chroot dpkg-query -W --showformat='${Package} ${Version}\n' | sudo tee image/casper/filesystem.manifest
+
+sudo cp -v image/casper/filesystem.manifest image/casper/filesystem.manifest-desktop
+
+sudo sed -i '/ubiquity/d' image/casper/filesystem.manifest-desktop
+sudo sed -i '/casper/d' image/casper/filesystem.manifest-desktop
+sudo sed -i '/discover/d' image/casper/filesystem.manifest-desktop
+sudo sed -i '/laptop-detect/d' image/casper/filesystem.manifest-desktop
+sudo sed -i '/os-prober/d' image/casper/filesystem.manifest-desktop
+
+sudo mksquashfs chroot image/casper/filesystem.squashfs
+
+printf $(sudo du -sx --block-size=1 chroot | cut -f1) > image/casper/filesystem.size
+
+cat <<EOF > image/README.diskdefines
+#define DISKNAME  Ubuntu from scratch
+#define TYPE  binary
+#define TYPEbinary  1
+#define ARCH  amd64
+#define ARCHamd64  1
+#define DISKNUM  1
+#define DISKNUM1  1
+#define TOTALNUM  0
+#define TOTALNUM0  1
+EOF
+
+cd $ISO_ROOT/image
+
+grub-mkstandalone \
+   --format=x86_64-efi \
+   --output=isolinux/bootx64.efi \
+   --locales="" \
+   --fonts="" \
+   "boot/grub/grub.cfg=isolinux/grub.cfg"
+
+(
+   cd isolinux && \
+   dd if=/dev/zero of=efiboot.img bs=1M count=10 && \
+   sudo mkfs.vfat efiboot.img && \
+   LC_CTYPE=C mmd -i efiboot.img efi efi/boot && \
+   LC_CTYPE=C mcopy -i efiboot.img ./bootx64.efi ::efi/boot/
+)
+
+grub-mkstandalone \
+   --format=i386-pc \
+   --output=isolinux/core.img \
+   --install-modules="linux16 linux normal iso9660 biosdisk memdisk search tar ls" \
+   --modules="linux16 linux normal iso9660 biosdisk search" \
+   --locales="" \
+   --fonts="" \
+   "boot/grub/grub.cfg=isolinux/grub.cfg"
+
+cat /usr/lib/grub/i386-pc/cdboot.img isolinux/core.img > isolinux/bios.img
+
+sudo /bin/bash -c "(find . -type f -print0 | xargs -0 md5sum | grep -v "\./md5sum.txt" > md5sum.txt)"
+
+sudo xorriso \
+   -as mkisofs \
+   -iso-level 3 \
+   -full-iso9660-filenames \
+   -volid "Ubuntu from scratch" \
+   -eltorito-boot boot/grub/bios.img \
+   -no-emul-boot \
+   -boot-load-size 4 \
+   -boot-info-table \
+   --eltorito-catalog boot/grub/boot.cat \
+   --grub2-boot-info \
+   --grub2-mbr /usr/lib/grub/i386-pc/boot_hybrid.img \
+   -eltorito-alt-boot \
+   -e EFI/efiboot.img \
+   -no-emul-boot \
+   -append_partition 2 0xef isolinux/efiboot.img \
+   -output "../ubuntu-from-scratch.iso" \
+   -graft-points \
+      "." \
+      /boot/grub/bios.img=isolinux/bios.img \
+      /EFI/efiboot.img=isolinux/efiboot.img
