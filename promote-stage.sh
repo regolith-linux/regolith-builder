bfffc02ed867c1a424a2d0029c1a8fcf77e89a68
Ken Gilmer <kgilmer@gmail.com>
2021-04-08 08:07:20 -0700

    Add package model for regolith 1.6

--- promote-stage.sh
+++ promote-stage.sh
@@ -12,7 +12,7 @@ set -e
 SOURCE_STAGE="~regolith-linux/ubuntu/$1"
 TARGET_STAGE="~regolith-linux/ubuntu/$2"
 if [ -z "$3" ]; then
-    PACKAGE_SPEC="arc-icon-theme,ayu-theme,cahuella,compton,fonts-jetbrains-mono,fonts-source-code-pro-ttf,i3-gaps-wm,i3-snapshot,i3ipc-python,i3xrocks,moka-icon-theme,nordic,paper-icon-theme,playerctl,regolith-compositor-compton-glx,regolith-compositor-none,regolith-compositor-xcompmgr,regolith-desktop,regolith-ftue,regolith-gdm3-theme,regolith-gnome-flashback,regolith-i3-gaps-config,regolith-i3xrocks-config,regolith-lightdm-config,regolith-rofi-config,regolith-rofication,regolith-st,regolith-styles,regolith-system,remontoire,rofi,solarc-theme,ubiquity-slideshow-ubuntu,unclutter-xfixes,xrescat"
+    PACKAGE_SPEC="arc-icon-theme,ayu-theme,cahuella,compton,fonts-jetbrains-mono,fonts-source-code-pro-ttf,i3-gaps-wm,i3-snapshot,i3ipc-python,i3xrocks,moka-icon-theme,nordic,paper-icon-theme,playerctl,regolith-compositor-compton-glx,regolith-compositor-none,regolith-compositor-xcompmgr,regolith-desktop,regolith-ftue,regolith-gdm3-theme,regolith-gnome-flashback,regolith-i3-gaps-config,regolith-i3xrocks-config,regolith-lightdm-config,regolith-rofi-config,regolith-rofication,regolith-default-settings,regolith-st,regolith-styles,regolith-system,remontoire,rofi,solarc-theme,ubiquity-slideshow-ubuntu,unclutter-xfixes,xrescat"
 else
     PACKAGE_SPEC=$3
 fi
--- promote.sh
+++ promote.sh
@@ -1,37 +1,21 @@
 #!/bin/bash
 
 # Input Parameters
-if [ "$#" -lt 3 ]; then
-    echo "Usage: promote.sh <source stage> <target stage> <package name>"
+if [ "$#" -lt 2 ]; then
+    echo "Usage: promote.sh <source stage> <target stage> [package name]"
     exit 1
 fi 
 
 SOURCE_STAGE="~regolith-linux/ubuntu/$1"
 TARGET_STAGE="~regolith-linux/ubuntu/$2"
-PACKAGE_NAME=$3
-
-FIRST_VERSION="focal"
-SECOND_VERSION="bionic"
-# THIRD_VERSION="eoan"
-
-BUILDER_GRACE_PERIOD=1200 # 20 minutes
-
-if [[ "$SOURCE_STAGE" == "$TARGET_STAGE" ]]; then
-    echo "Copying $PACKAGE_NAME version $FIRST_VERSION to $SECOND_VERSION in $SOURCE_STAGE"
-    ./back-copy.sh $SOURCE_STAGE $FIRST_VERSION $TARGET_STAGE $SECOND_VERSION $PACKAGE_NAME
-
-    # PPAs cannot copy multiple releaes of the same package concurrently.
-    #sleep $BUILDER_GRACE_PERIOD
-
-    #echo "Copying $PACKAGE_NAME version $FIRST_VERSION to $THIRD_VERSION in $SOURCE_STAGE"
-    #./back-copy.sh $SOURCE_STAGE $FIRST_VERSION $TARGET_STAGE $THIRD_VERSION $PACKAGE_NAME
-else 
-    echo "Promoting $PACKAGE_NAME from $SOURCE_STAGE to $TARGET_STAGE version $FIRST_VERSION"
-    ./back-copy.sh $SOURCE_STAGE $FIRST_VERSION $TARGET_STAGE $FIRST_VERSION $PACKAGE_NAME
-
-    # PPAs cannot copy multiple releaes of the same package concurrently.
-    sleep $BUILDER_GRACE_PERIOD
-
-    echo "Promoting $PACKAGE_NAME from $SOURCE_STAGE to $TARGET_STAGE version $SECOND_VERSION"
-    ./back-copy.sh $SOURCE_STAGE $SECOND_VERSION $TARGET_STAGE $SECOND_VERSION $PACKAGE_NAME
+if [ -z "$3" ]; then
+    PACKAGE_SPEC="regolith-default-settings,i3-next-workspace,regolith-todo,regolith-compositor-picom-glx,dracula-gtk,gruvbox-gtk,plano-theme,regolith-unclutter-xfixes,arc-icon-theme,ayu-theme,cahuella,compton,fonts-jetbrains-mono,fonts-source-code-pro-ttf,i3-gaps-wm,i3-snapshot,i3ipc-python,i3xrocks,moka-icon-theme,nordic,paper-icon-theme,playerctl,regolith-compositor-compton-glx,regolith-compositor-none,regolith-compositor-xcompmgr,regolith-desktop,regolith-ftue,regolith-gdm3-theme,regolith-gnome-flashback,regolith-i3-gaps-config,regolith-i3xrocks-config,regolith-lightdm-config,regolith-rofi-config,regolith-rofication,regolith-st,regolith-styles,regolith-system,remontoire,rofi,solarc-theme,ubiquity-slideshow-ubuntu,unclutter-xfixes,xrescat"
+else
+    PACKAGE_SPEC=$3
 fi
+
+FIRST_VERSION="groovy"
+SECOND_VERSION="hirsute"
+
+echo "Copying $PACKAGE_NAME version $FIRST_VERSION to $SECOND_VERSION in $SOURCE_STAGE"
+./back-copy.sh $SOURCE_STAGE $FIRST_VERSION $TARGET_STAGE $SECOND_VERSION $PACKAGE_SPEC
