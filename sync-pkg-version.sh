f66493e13f175af1afde5ac9eb01869350cab25c
Ken Gilmer <kgilmer@gmail.com>
2021-04-29 22:11:13 -0700

    model updates and deprecate groovy

--- sync-pkg-version.sh
+++ sync-pkg-version.sh
@@ -11,14 +11,13 @@ PACKAGE_NAME=$2
 
 FIRST_VERSION="bionic"
 SECOND_VERSION="focal"
-THIRD_VERSION="groovy"
+THIRD_VERSION="hirsute"
 
 BUILDER_GRACE_PERIOD=1200 # 20 minutes
 
 echo "Copying $PACKAGE_NAME version $FIRST_VERSION to $SECOND_VERSION in $STAGE"
 ./back-copy.sh $STAGE $FIRST_VERSION $STAGE $SECOND_VERSION $PACKAGE_NAME
 
-# PPAs cannot copy multiple releaes of the same package concurrently.
 sleep $BUILDER_GRACE_PERIOD
 
 echo "Copying $PACKAGE_NAME version $FIRST_VERSION to $THIRD_VERSION in $STAGE"
